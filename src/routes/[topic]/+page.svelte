beb48f2fe8cc8176fa22cec0e518c899e43bc853
Stoffei07 <resch.stefan@johak.at>
2024-11-12 11:43:15 +0100

    Update +page.svelte

--- src/routes/[topic]/+page.svelte
+++ src/routes/[topic]/+page.svelte
@@ -3,24 +3,22 @@
 	import ListItem from '$lib/fonts/components/ListItem.svelte';
 	let { data } = $props();
 	let currentQuestion = $state(0);
-	let score = $state(0); //min 7:15 stehengeblieben
+	let score = $state(0);
 	let selectedAnswer = $state('');
 	let submitted = $state(false);
 	let progressValue = $state(0);
 
 	function handleSubmit() {
-		data.questions[currentQuestion].selectedAnswer = selectedAnswer;
 		submitted = true;
+		data.questions[currentQuestion].selectedAnswer = selectedAnswer;
 		if (selectedAnswer === data.questions[currentQuestion].answer) {
 			score++;
-			progressValue += 10;
 		}
-		currentQuestion++;
 	}
 
 	function nextQuestion() {
-		currentsQuestion++;
-		// submitted = false;
+		currentQuestion++;
+		submitted = false;
 	}
 </script>
 
@@ -35,6 +33,7 @@
 				<ListItem
 					title={option}
 					{index}
+					correctAnswer={}
 					focusAction={() => (selectedAnswer = option)}
 					correctAnswer={question.selectedAnswer && question.answer === option}
 				></ListItem>
@@ -43,15 +42,13 @@
 	{/if}
 {/each}
 
-<!-- {#if !submitted} -->
-<button class="btn btn-primary" onclick={handleSubmit}> Submit </button>
-<button class="btn btn-primary" onclick={nextQuestion}> Next Question </button>
+{#if !submitted}
+	<button class="btn btn-primary" onclick={handleSubmit}> Submit </button>
+{:else}
+	<button class="btn btn-primary" onclick={nextQuestion}> Next Question </button>
+{/if}
+{selectedAnswer}
 {score}
-
-<progress class="progress w-56" value={progressValue} max="100"></progress>
-
-<!-- {:else} -->
-<!-- <button class="btn btn-primary" onclick={nextQuestion}> Next Question </button> -->
-<!-- {/if} -->
-
-<!-- {JSON.stringify(data.questions[currentQuestion])} -->
+{#key submitted}
+	{JSON.stringify(data.questions[currentQuestion])}
+{/key}
